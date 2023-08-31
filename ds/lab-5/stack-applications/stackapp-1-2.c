d8f496b9ee245e1ed89c029b48d955128ed02e88
Sudibyo Sinha <88853543+ScalarJerk@users.noreply.github.com>
2023-08-31 10:01:43 +0530

    Refactor stackapp-1-2.c

--- ds/lab-5/stack-applications/stackapp-1-2.c
+++ ds/lab-5/stack-applications/stackapp-1-2.c
@@ -1,88 +1,115 @@
-/* Write a program to evaluate a postfix 
-expression. The input to the program is a postfix 
-expression.*/
-  
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define MAX_SIZE 100
-
+// Structure for Stack
 struct Stack {
-    int top;
-    int items[MAX_SIZE];
+	int top;
+	int capacity;
+	int* arr;
 };
 
-void initialize(struct Stack *stack) {
-    stack->top = -1;
+// Function to create a stack
+struct Stack* createStack(int cap) {
+	struct Stack* stack = (struct Stack*)malloc(sizeof(struct Stack));
+	if (!stack)
+		return NULL;
+
+	stack->top = -1;
+	stack->capacity = cap;
+	stack->arr = (int*)malloc(stack->capacity * sizeof(int));
+	if (!stack->arr)
+		return NULL;
+
+	return stack;
 }
 
-int isEmpty(struct Stack *stack) {
-    return stack->top == -1;
+// Check if stack is empty
+int isEmpty(struct Stack* stack) {
+	return stack->top == -1;
 }
 
-int isFull(struct Stack *stack) {
-    return stack->top == MAX_SIZE - 1;
+// Get the top element of the stack
+int peek(struct Stack* stack) {
+	return stack->arr[stack->top];
 }
 
-void push(struct Stack *stack, int item) {
-    if (isFull(stack)) {
-        printf("Stack is full. Cannot push element.\n");
-    } else {
-        stack->items[++stack->top] = item;
-    }
+// Pop operation
+int pop(struct Stack* stack) {
+	if (!isEmpty(stack))
+		return stack->arr[stack->top--];
+	return -1; // Return an invalid value to indicate error
 }
 
-int pop(struct Stack *stack) {
-    if (isEmpty(stack)) {
-        printf("Stack is empty. Cannot pop element.\n");
-        return -1;
-    } else {
-        return stack->items[stack->top--];
-    }
+// Push operation
+void push(struct Stack* stack, int val) {
+	stack->arr[++stack->top] = val;
 }
 
-int evaluatePostfix(char postfix[]) {
-    struct Stack stack;
-    initialize(&stack);
+// Evaluate postfix expression
+int evaluatePostfix(char* exp) {
+	// Create a stack of capacity equal to expression size
+	struct Stack* stack = createStack(strlen(exp));
+	int i;
 
-    int i = 0;
-    while (postfix[i] != '\0') {
-        if (postfix[i] >= '0' && postfix[i] <= '9') {
-            push(&stack, postfix[i] - '0'); // Convert char digit to integer
-        } else {
-            int operand2 = pop(&stack);
-            int operand1 = pop(&stack);
-            
-            switch (postfix[i]) {
-                case '+':
-                    push(&stack, operand1 + operand2);
-                    break;
-                case '-':
-                    push(&stack, operand1 - operand2);
-                    break;
-                case '*':
-                    push(&stack, operand1 * operand2);
-                    break;
-                case '/':
-                    push(&stack, operand1 / operand2);
-                    break;
-            }
-        }
-        i++;
-    }
+	if (!stack)
+		return -1; // Return an invalid value to indicate error
 
-    return pop(&stack);
+	// Scan all characters one by one
+	for (i = 0; exp[i]; ++i) {
+		// If the scanned character is an operand (number here),
+		// push it to the stack.
+		if (isdigit(exp[i]))
+			push(stack, exp[i] - '0');
+		else {
+			// If the scanned character is an operator,
+			// pop two elements from the stack and apply the operator.
+			int val1 = pop(stack);
+			int val2 = pop(stack);
+			switch (exp[i]) {
+			case '+':
+				push(stack, val2 + val1);
+				break;
+			case '-':
+				push(stack, val2 - val1);
+				break;
+			case '*':
+				push(stack, val2 * val1);
+				break;
+			case '/':
+				push(stack, val2 / val1);
+				break;
+			}
+		}
+	}
+	return pop(stack); // The final result is left at the top of the stack
 }
 
 int main() {
-    char postfix[MAX_SIZE];
+	char exp[50];
+	int result;
 
-    printf("Enter a postfix expression: ");
-    scanf("%s", postfix);
+	do {
+		// Input postfix expression from user
+		printf("Enter a postfix expression: ");
+		scanf("%s", exp);
 
-    int result = evaluatePostfix(postfix);
-    printf("Result: %d\n", result);
+		// Evaluate the expression
+		result = evaluatePostfix(exp);
+		if (result != -1)
+			printf("Result: %d\n", result);
+		else
+			printf("Invalid expression\n");
 
-    return 0;
+		// Ask user if they want to enter another input
+		char choice;
+		printf("Do you want to evaluate another expression? (y/n): ");
+		scanf(" %c", &choice);
+
+		if (choice != 'y' && choice != 'Y')
+			break;
+
+	} while (1);
+
+	return 0;
 }
